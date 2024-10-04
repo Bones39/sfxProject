7d0f8f41a5ec06ac1100c8d7b303a921102294e7
ANDRIAMISEZA Andraina <aandriamiseza@lateca.fr>
2024-10-04 14:09:15 +0200

    add grabing instruction on SX side

--- index.js
+++ index.js
@@ -18,6 +18,18 @@ document.querySelector('[name="plt_02/10/2024_0"]').children[2].selected = true
 
 // pour mettre une valeur dans "choisir une affaire"
 document.querySelector('#comboprj_1').value = "test"
+document.querySelector('#comboprj_1').value = "ESKER SA INT_OP-ATG-24-0001-ATG Dispositif ESKER 2024"
+document.querySelector('#comboprj_1').className = 'x-form-text x-form-field tisCombo'
 
 // pour mettre une valeur dans "choisir une tache" sur la 2nd ligne
-document.querySelector('#combolev_2').value = "test3"
+document.querySelector('#combolev_1').value = "test3"
+document.querySelector('#combolev_1').value = "Dispositif Esker ATG 2024 Ingénieur Etudes et Développement Consultant technique Confirmé"
+// afficher la ligne 3 "sous tache"
+document.querySelector('#ext-gen222').className = 'x-form-field-wrap' // ald 'x-form-field-wrap  x-hide-offsets'
+
+
+// la liste des valeur pour "choisir une affaire"
+document.querySelector('.x-combo-list-inner')
+
+// pour afficher le combo
+document.querySelector('#ext-gen209').className = 'x-form-field-wrap' // ald 'x-form-field-wrap  x-hide-offsets'
