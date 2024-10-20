13d20a459ab84d34b7f682f152a813570940d7bd
ANDRIAMISEZA Andraina <aandriamiseza@lateca.fr>
2024-10-20 19:26:06 +0200

    log in SX

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// -------- SF -------
+/* // -------- SF -------
 
 // modifier la premiere colonne de la premiere ligne (mettre 5):
 // il suffit de concatener le nom des classes dans querySelector
@@ -24,6 +24,10 @@ document.querySelector('#comboprj_1').className = 'x-form-text x-form-field tisC
 // 2 pour mettre une valeur dans "choisir une tache" sur la 2nd ligne
 document.querySelector('#combolev_1').value = "test3"
 document.querySelector('#combolev_1').value = "Dispositif Esker ATG 2024 Ingénieur Etudes et Développement Consultant technique Confirmé"
+// set le input cachée
+document.querySelector('#lev_1').value = "276701"
+// pour afficher le combo
+document.querySelector('#ext-gen209').className = 'x-form-field-wrap' // ald 'x-form-field-wrap  x-hide-offsets'
 // 3 afficher la ligne 3 "sous tache"
 document.querySelector('#ext-gen222').className = 'x-form-field-wrap' // ald 'x-form-field-wrap  x-hide-offsets'
 // 4 mettre une valeur dans les temps
@@ -32,5 +36,41 @@ document.querySelector('[name="plt_02/10/2024_1"]').children[2].selected = true
 // la liste des valeur pour "choisir une affaire"
 document.querySelector('.x-combo-list-inner')
 
-// pour afficher le combo
-document.querySelector('#ext-gen209').className = 'x-form-field-wrap' // ald 'x-form-field-wrap  x-hide-offsets'
+// passer par edge et utiliser le presse papier (ça evite de passer par la console) */
+
+const puppeteer = require('puppeteer-extra');
+const stealthPluggin = require('puppeteer-extra-plugin-stealth');
+const { executablePath } = require('puppeteer');
+require('dotenv').config()
+
+// puppeteer.use(stealthPluggin());
+
+async function navigateSx() {
+    const browser = await puppeteer.launch({
+        headless: false,
+        executablePath: executablePath()
+    });
+    
+    
+    try {
+        const page = await browser.newPage();
+        await page.goto('https://erp.alteca.fr/sx/xLogin');
+        // page.waitForSelector('form');
+        // page.type('input[name="login"]', 'aandriamiseza');
+        try {
+            await page.type('#login', process.env.LOGIN);
+            await page.type('input[name="password"]', process.env.PASSWORD);
+            await page.click('button[type="submit"]');
+        } catch (error) {
+            console.log(error);
+        }
+        // submit the form
+    
+        // Promise.all([page.waitForNavigation(), page.waitForSelector('#NXLeftMenu'), page.waitForSelector('#NXTopMenu')]);
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+navigateSx();
